4d1f2b298c77bb6c6f6a907170c21bd04fa55812
Vasiliy <localducks@gmail.com>
2017-07-05 22:31:48 +0300

    Text token statistics

---
+++ src/main/kotlin/com/pdf/textparser/PdfTextOperations.kt
@@ -0,0 +1,47 @@
+package com.pdf.textparser
+
+import org.springframework.stereotype.Service
+
+
+interface TextOperations {
+
+    fun getListOfTokens(text: String, regexp: Regex = "[A-Za-z]+".toRegex()): List<MatchResult>
+
+    fun filterListOfTokens(tokens: List<MatchResult>): List<String>
+
+    fun getWordStatisticsMap(tokens: List<String>): Map<String, Int>
+
+}
+
+@Service
+class SimpleTextOperations : TextOperations {
+
+    override fun getListOfTokens(text: String, regexp: Regex): List<MatchResult> = regexp.findAll(text).toList()
+
+
+    override fun filterListOfTokens(tokens: List<MatchResult>): List<String> = tokens
+            .map { it.value }
+            .map {
+                when {
+                    it.length == 1 -> ""
+                    it[0].isUpperCase() && it[1].isUpperCase() -> it
+                    else -> it.toLowerCase()
+                }
+            }.filter { it != "" }
+
+
+
+
+    override fun getWordStatisticsMap(tokens: List<String>): Map<String, Int> {
+        val wordCountMap = hashMapOf<String, Int>()
+
+        tokens.forEach {
+            if (!wordCountMap.containsKey(it))
+                wordCountMap.put(it, 1)
+            else
+                wordCountMap.put(it, (wordCountMap[it]!! + 1))
+        }
+
+        return wordCountMap
+    }
+}
--- src/test/kotlin/com/pdf/textparser/DemoApplicationTests.kt
+++ src/test/kotlin/com/pdf/textparser/DemoApplicationTests.kt
@@ -6,7 +6,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import kotlin.text.forEach
+import java.util.Comparator
+import java.util.function.ToIntFunction
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -23,22 +24,61 @@ class DemoApplicationTests {
 
     @Test
     fun readTextFromPdfDocumentTest() {
-       val regexp = "[A-Za-z]+".toRegex()
-
+        val regexp = "[A-Za-z]+".toRegex()
 
         val pdfDocument = pdDocument()
         pdfDocument.use {
-          val pdfText = readPdfDocumentText(pdfDocument)
-          println("Letters of file ZeroMQ =  ${pdfText.length}")
-        //  println("Length of file ZeroMQ =  ${pdfText.slice(IntRange(0,15000))}")
-        //  println("Length of file ZeroMQ =  ${pdfText.filter({s:Char -> s.isLetter().and()})}")
-          val listOfWords: Sequence<MatchResult> = regexp.findAll(pdfText)
-          println(listOfWords.count())
-          println(listOfWords.toList().forEach({println(it.value)}))
+            val pdfText = readPdfDocumentText(pdfDocument)
+            //   println("Number of Letters of file ZeroMQ =  ${pdfText.length}")
+            //   println("Letters of file ZeroMQ =  ${pdfText.decapitalize()}")
+
+            val wordCountMap = hashMapOf<String, Int>()
+
+            val listOfWords = regexp.findAll(pdfText).toList()
+
+            val processWords = listOfWords
+                    .map { it.value }
+                    .map {
+                        when {
+                            it.length == 1 -> ""
+                            it[0].isUpperCase() && it[1].isUpperCase() -> it
+                            else -> it.toLowerCase()
+                        }
+                    }.filter { it != "" }
+
+
+            processWords.forEach {
+                if (!wordCountMap.containsKey(it))
+                    wordCountMap.put(it, 1)
+                else
+                    wordCountMap.put(it, (wordCountMap[it]!! + 1))
+            }
+
+            println(wordCountMap.values.sortedDescending())
+            val sortedCountWords = wordCountMap.values.sortedDescending()
+
+            println(wordCountMap.entries.sortedWith(Comparator.comparingInt({-it.value})))
+
+
+            val setOfWords = processWords.toSet()
+
+
+
+
+
+            println(setOfWords.size)
+            println(processWords.size)
+
+
+            // println(if (word.value.toList()[1].isUpperCase()) word.value else word.value.decapitalize())
+
+            //println(listOfWords.toList().forEach({println(it.value)}))
             //  val words: MatchResult? = regexp.find(pdfText)
-        //  println("Words in ZeroMQ = ${listOfWords.size}")
-          // print(words?.next().toString())
-            listOfWords.onEach { print(it.groupValues) }
+            //  println("Words in ZeroMQ = ${listOfWords.size}")
+            // print(words?.next().toString())
+
+
+            //   listOfWords.onEach { print(it.groupValues) }
 
         }
     }

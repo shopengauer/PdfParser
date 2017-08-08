9c9aebbd5af51ac4111a003aa9602ac5c8c7776b
vasilij <localducks@gmail.com>
2017-08-08 16:30:41 +0300

    Json object send to client

--- src/main/kotlin/com/pdf/textparser/DemoApplication.kt
+++ src/main/kotlin/com/pdf/textparser/DemoApplication.kt
@@ -1,12 +1,8 @@
 package com.pdf.textparser
 
-import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.pdmodel.PDPage
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.stereotype.Repository
-import java.io.File
 
 @SpringBootApplication
 @ComponentScan
@@ -15,44 +11,3 @@ class DemoApplication
 fun main(args: Array<String>) {
     SpringApplication.run(DemoApplication::class.java, *args)
 }
-
-@Repository
-class PdfReader : PdfFileOperation {
-
-    override fun createBlankPdf() = PDDocument()
-
-    override fun createBlankPdfFile(fileName: String) {
-        createBlankPdf()
-    }
-
-    fun readPdfFile(fileName: String): PDDocument? {
-        val pdfDocument = PDDocument.load(File(fileName))
-        return PDDocument()
-    }
-
-
-    val prefix = "~/"
-
-    fun createBlankPdf(fileName: String) {
-        val doc = PDDocument()
-        doc.use {
-            val pdPage = PDPage()
-            doc.addPage(pdPage)
-            print("${System.getProperty("user.home")}${File.separator}$fileName")
-          //  doc.save(getHomePath(fileName))
-        }
-    }
-
-
-    fun getHomePath(fileName: String) = "${System.getProperty("user.home")}${File.separator}$fileName"
-
-}
-
-interface PdfFileOperation {
-
-    fun createBlankPdf(): PDDocument
-
-    fun createBlankPdfFile(fileName: String)
-
-}
-
--- src/main/kotlin/com/pdf/textparser/platform/api_verticles.kt
+++ src/main/kotlin/com/pdf/textparser/platform/api_verticles.kt
@@ -1,8 +1,11 @@
 package com.pdf.textparser.platform
 
+import com.pdf.textparser.businesslogic.Word
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.Handler
 import io.vertx.core.http.HttpServer
+import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
@@ -17,13 +20,11 @@ class ApiVerticle : AbstractVerticle() {
         val apiServer: HttpServer = vertx.createHttpServer()
         val router: Router = Router.router(vertx)
         router.get().path("/word").handler({ context ->
-            context.response().putHeader("content-type", "application/json")
-            context.response().end(JsonObject().put("book", "Kotlin in action")
-                    .put("words", JsonObject().put("emerge","появляться").put("study","учиться"))
-                    .encodePrettily())
+            context.response().putHeader("content-type", "application/json;charset=utf-8")
+            context.response().end(Json.encodePrettily(listOf(Word("hello", setOf("привет")))))
         })
 
-        apiServer.requestHandler { router.accept(it) }.listen(8081)
+        apiServer.requestHandler(router::accept).listen(8081, {asyncResult -> println(asyncResult.succeeded()) })
 
     }
 }

f9eec99e2527caca7be86b6861bc052ed0d57476
vasilij <localducks@gmail.com>
2017-08-12 20:16:49 +0300

    File upload form

--- dartweb/lib/app_component.dart
+++ dartweb/lib/app_component.dart
@@ -4,7 +4,7 @@
 import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
-import 'src/todo_list/todo_list_component.dart';
+import 'package:dartweb/src/file_upload/file_upload_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -13,7 +13,7 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
+  directives: const [materialDirectives, FileUploadComponent],
   providers: const [materialProviders],
 )
 class AppComponent {
---
+++ dartweb/lib/src/file_upload/file_upload_component.dart
@@ -0,0 +1,12 @@
+
+import 'package:angular2/angular2.dart';
+
+@Component(
+  selector: 'file-upload',
+  templateUrl: 'file_upload_component.html'
+)
+class FileUploadComponent{
+
+
+
+}
